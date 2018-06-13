be0503f78b48d5ca7caa10390b514f2b24342d4f
Cristiano Costa <cristiano@cristianocosta.me>
2018-06-12 22:36:43 -0300

    Bug fixes in OPTIONS

--- optionsHandler.go
+++ optionsHandler.go
@@ -7,11 +7,11 @@ import (
 
 func OptionsHandler(headers []string) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		r.Header.Add("Access-Control-Allow-Origin", "*")
-		r.Header.Add("Access-Control-Allow-Credentials", "true")
-		r.Header.Add("Access-Control-Allow-Methods", strings.Join(headers, ", "))
-		r.Header.Add("Access-Control-Allow-Headers", "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization")
-		r.Header.Add("Access-Control-Max-Age", "1728000")
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Credentials", "true")
+		w.Header().Add("Access-Control-Allow-Methods", strings.Join(headers, ", "))
+		w.Header().Add("Access-Control-Allow-Headers", "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization")
+		w.Header().Add("Access-Control-Max-Age", "1728000")
 
 		SendEmptyResponse(w, http.StatusOK)
 	})
