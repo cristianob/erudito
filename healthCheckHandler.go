e93b848da9b4aa7b3d685baa90e64d2fbfffee2c
Cristiano Costa <cristiano@cristianocosta.me>
2019-03-26 21:38:57 -0300

    Changed Health Check Response

--- healthCheckHandler.go
+++ healthCheckHandler.go
@@ -8,12 +8,6 @@ func HealthCheckHandler(maestro *maestro) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		AddCORSHeaders(w, "GET")
 
-		db := maestro.dBPoolCallback(r)
-		if db == nil {
-			SendSingleError(w, http.StatusInternalServerError, "Database error!", "DATABASE_ERROR")
-			return
-		}
-
-		SendEmptyResponse(w, http.StatusOK)
+		SendData(w, http.StatusOK, "OK")
 	})
 }
